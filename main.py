aab90319b529031224ff1773535daeda1ec6450b
AvenCores <avencores@gmail.com>
2023-04-25 19:40:03 +0300

    upd

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ def clear():
 	else:
 		os.system("clear")
 
-clear()
 colors = ['red', 'yellow', 'green', 'cyan', 'light_blue', 'magenta']
 color_index = 0
 bannercli ="""                                                   /$$                   /$$ /$$
@@ -29,15 +28,19 @@ bannercli ="""                                                   /$$            
           | $$                                                                  
           |__/    """
 
-for i in range(len(bannercli)):
-    color = colors[i % len(colors)]
-    print(colored(bannercli[i], color), end="")
-print(colored("\n\nAuthor: ", "red") + "@avencores")
-print(colored("Telegram channel: ", "red") + "@hzfnews")
+def banner():
+    for i in range(len(bannercli)):
+        color = colors[i % len(colors)]
+        print(colored(bannercli[i], color), end="")
+    print(colored("\n\nAuthor: ", "red") + "@avencores")
+    print(colored("Telegram channel: ", "red") + "@hzfnews")
+
+clear()
+banner()  
 config = configparser.ConfigParser()
 config.read('config.ini')
 if 'openai-token' not in config:
-    token = input(colored("\nEnter your token: ", "yellow"))
+    token = input(colored("\nEnter your token ", "yellow") + colored(">> ", "green"))
     config['openai-token'] = {'token': token}
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
@@ -51,109 +54,79 @@ def menu():
     while True:
         openai.api_key = token
         clear()
-        for i in range(len(bannercli)):
-            color = colors[i % len(colors)]
-            print(colored(bannercli[i], color), end="")
-        print(colored("\n\nAuthor: ", "red") + "@avencores")
-        print(colored("Telegram channel: ", "red") + "@hzfnews")
-        print(colored("\n1) ", "cyan") + "ChatGPT")
-        print(colored("2) ", "cyan") + "DALLE-2")
-        print(colored("3) ", "cyan") + "Whisper")
-        print(colored("\n4) ", "cyan") + "Change openai token")
-        print(colored("5) ", "cyan") + "Change openai session key")
-        print(colored("\n6) ", "cyan") + "Check token balance")
-        print(colored("\n0) ", "cyan") + "Exit")
-        choice = input(colored("\nEnter your choice (0-6): ", "yellow"))
+        banner()
+        print(colored("\n[1] ", "cyan") + "ChatGPT")
+        print(colored("[2] ", "cyan") + "DALLE-2")
+        print(colored("[3] ", "cyan") + "Whisper")
+        print(colored("\n[4] ", "cyan") + "Change openai token")
+        print(colored("[5] ", "cyan") + "Change openai session key")
+        print(colored("\n[6] ", "cyan") + "Check token balance")
+        print(colored("\n[0] ", "cyan") + "Exit")
+        choice = input(colored("\nEnter your choice (0-6) ", "yellow") + colored(">> ", "green"))
         if choice == "0":
             clear()
             print(colored("Bye!", "cyan"))
             exit()
         if choice == "1":
             clear()
-            for i in range(len(bannercli)):
-                color = colors[i % len(colors)]
-                print(colored(bannercli[i], color), end="")
-            print(colored("\n\nAuthor: ", "red") + "@avencores")
-            print(colored("Telegram channel: ", "red") + "@hzfnews")
-            print(colored("\n1) ", "cyan") + "ChatGPT 4.0")
-            print(colored("2) ", "cyan") + "ChatGPT 3.5")
-            choice = input(colored("\nEnter your choice (1-2): ", "yellow"))
+            banner()
+            print(colored("\n[1] ", "cyan") + "ChatGPT 4.0")
+            print(colored("[2] ", "cyan") + "ChatGPT 3.5")
+            choice = input(colored("\nEnter your choice (1-2) ", "yellow") + colored(">> ", "green"))
             if choice == "1":
                 clear()
-                for i in range(len(bannercli)):
-                    color = colors[i % len(colors)]
-                    print(colored(bannercli[i], color), end="")
-                print(colored("\n\nAuthor: ", "red") + "@avencores")
-                print(colored("Telegram channel: ", "red") + "@hzfnews")
+                banner()
                 print(colored("\nChatGPT Model: ", "light_blue") + "4.0")
-                resp = input(colored("\nEnter your request (leave blank to keep existing): ", "yellow"))
+                resp = input(colored("\nEnter your request (leave blank to keep existing) ", "yellow") + colored(">> ", "green"))
                 if resp:
                     try:
                         response = openai.ChatCompletion.create(
                         model="gpt-4",
                         messages=[{"role": "user", "content": resp}])
                         output = response["choices"][0]["message"]["content"]
-                        print(colored(f"\nResponse from ChatGPT 4.0: ", "green") + output)
+                        print(colored(f"\nResponse from ChatGPT 4.0 ", "green") + colored(">> ", "green") + output)
                         print(colored("\nPress ENTER to return to the main menu", "cyan"))
                         input()
                     except Exception as e:
                         clear()
-                        for i in range(len(bannercli)):
-                            color = colors[i % len(colors)]
-                            print(colored(bannercli[i], color), end="")
-                        print(colored("\n\nAuthor: ", "red") + "@avencores")
-                        print(colored("Telegram channel: ", "red") + "@hzfnews")
+                        banner()
                         print(colored("\nERROR: ", "yellow") + str(e))
                         print(colored("\nPress ENTER to return to the main menu", "cyan"))
                         input()
                         menu()
+                else:
+                    menu()
             if choice == "2":
                 clear()
-                for i in range(len(bannercli)):
-                    color = colors[i % len(colors)]
-                    print(colored(bannercli[i], color), end="")
-                print(colored("\n\nAuthor: ", "red") + "@avencores")
-                print(colored("Telegram channel: ", "red") + "@hzfnews")
+                banner()
                 print(colored("\nChatGPT Model: ", "light_blue") + "3.5")
-                resp = input(colored("\nEnter your request (leave blank to keep existing): ", "yellow"))
+                resp = input(colored("\nEnter your request (leave blank to keep existing) ", "yellow") + colored(">> ", "green"))
                 if resp:
                     try:
                         response = openai.ChatCompletion.create(
                         model="gpt-3.5-turbo",
                         messages=[{"role": "user", "content": resp}])
                         output = response["choices"][0]["message"]["content"]
-                        print(colored(f"\nResponse from ChatGPT 3.5: ", "green") + output)
+                        print(colored(f"\nResponse from ChatGPT 3.5 ", "green") + colored(">> ", "green") + output)
                         print(colored("\nPress ENTER to return to the main menu", "cyan"))
                         input()
                     except Exception as e:
                         clear()
-                        for i in range(len(bannercli)):
-                            color = colors[i % len(colors)]
-                            print(colored(bannercli[i], color), end="")
-                        print(colored("\n\nAuthor: ", "red") + "@avencores")
-                        print(colored("Telegram channel: ", "red") + "@hzfnews")
+                        banner()
                         print(colored("\nERROR: ", "yellow") + str(e))
                         print(colored("\nPress ENTER to return to the main menu", "cyan"))
                         input()
                         menu()
             else:
                 clear()
-                for i in range(len(bannercli)):
-                    color = colors[i % len(colors)]
-                    print(colored(bannercli[i], color), end="")
-                print(colored("\n\nAuthor: ", "red") + "@avencores")
-                print(colored("Telegram channel: ", "red") + "@hzfnews")
+                banner()
                 print(colored("\nInvalid choice. Please try again.", "light_blue"))
                 print(colored("\nPress ENTER to return to the main menu", "cyan"))
                 input()
         elif choice == "2":
             clear()
-            for i in range(len(bannercli)):
-                color = colors[i % len(colors)]
-                print(colored(bannercli[i], color), end="")
-            print(colored("\n\nAuthor: ", "red") + "@avencores")
-            print(colored("Telegram channel: ", "red") + "@hzfnews")
-            resp = input(colored("\nEnter your request (leave blank to keep existing): ", "yellow"))
+            banner()
+            resp = input(colored("\nEnter your request (leave blank to keep existing) ", "yellow") + colored(">> ", "green"))
             if resp:
                 try:
                     response = openai.Image.create(
@@ -161,57 +134,41 @@ def menu():
                         n=1,
                         size="1024x1024")
                     output = response['data'][0]['url']
-                    print(colored(f"\nResponse from DALLE-2: ", "green") + output)
+                    print(colored(f"\nResponse from DALLE-2 ", "green")  + colored(">> ", "green") + output)
                     print(colored("\nPress ENTER to return to the main menu", "cyan"))
                     input()
                 except Exception as e:
                     clear()
-                    for i in range(len(bannercli)):
-                        color = colors[i % len(colors)]
-                        print(colored(bannercli[i], color), end="")
-                    print(colored("\n\nAuthor: ", "red") + "@avencores")
-                    print(colored("Telegram channel: ", "red") + "@hzfnews")
+                    banner()
                     print(colored("\nERROR: ", "yellow") + str(e))
                     print(colored("\nPress ENTER to return to the main menu", "cyan"))
                     input()
                     menu()
         elif choice == "3":
             clear()
-            for i in range(len(bannercli)):
-                color = colors[i % len(colors)]
-                print(colored(bannercli[i], color), end="")
-            print(colored("\n\nAuthor: ", "red") + "@avencores")
-            print(colored("Telegram channel: ", "red") + "@hzfnews")
+            banner()
             print(colored("\nFile uploads are currently limited to 25 MB and the following input file types are supported: mp3, mp4, mpeg, mpga, m4a, wav, and webm.", "green"))
-            resp = input(colored("\nEnter the full path to the file (leave blank to keep existing): ", "yellow"))
+            resp = input(colored("\nEnter the full path to the file (leave blank to keep existing) ", "yellow") + colored(">> ", "green"))
             if resp:
                 try:
                     fileaudio = open(f"{resp}", "rb")
                     response = openai.Audio.transcribe("whisper-1", fileaudio)
                     fileaudio.close()
                     output = response["text"]
-                    print(colored(f"\nResponse from Whisper: ", "green") + output)
+                    print(colored(f"\nResponse from Whisper ", "green")  + colored(">> ", "green") + output)
                     print(colored("\nPress ENTER to return to the main menu", "cyan"))
                     input()
                 except Exception as e:
                     clear()
-                    for i in range(len(bannercli)):
-                        color = colors[i % len(colors)]
-                        print(colored(bannercli[i], color), end="")
-                    print(colored("\n\nAuthor: ", "red") + "@avencores")
-                    print(colored("Telegram channel: ", "red") + "@hzfnews")
+                    banner()
                     print(colored("\nERROR: ", "yellow") + str(e))
                     print(colored("\nPress ENTER to return to the main menu", "cyan"))
                     input()
                     menu()
         elif choice == "4":
             clear()
-            for i in range(len(bannercli)):
-                color = colors[i % len(colors)]
-                print(colored(bannercli[i], color), end="")
-            print(colored("\n\nAuthor: ", "red") + "@avencores")
-            print(colored("Telegram channel: ", "red") + "@hzfnews")
-            new_token = input(colored("\nEnter new token (leave blank to keep existing): ", "yellow"))
+            banner()
+            new_token = input(colored("\nEnter new token (leave blank to keep existing) ", "yellow") + colored(">> ", "green"))
             if new_token:
                 config['openai-token']['token'] = new_token
                 with open('config.ini', 'w') as configfile:
@@ -221,36 +178,25 @@ def menu():
                     exit()
         elif choice == "5":
             clear()
-            for i in range(len(bannercli)):
-                color = colors[i % len(colors)]
-                print(colored(bannercli[i], color), end="")
-            print(colored("\n\nAuthor: ", "red") + "@avencores")
-            print(colored("Telegram channel: ", "red") + "@hzfnews")
+            banner()
             print(colored("\nRead this article: ", "light_blue") + "https://lmmsoft.github.io/openai_update_check_balance_api/")
-            session_key = input(colored("\nEnter your session-key: ", "yellow"))
-            config['openai-key'] = {'session_key': session_key}
-            with open('config.ini', 'w') as configfile:
-                config.write(configfile)
-                clear()
-                print(colored("Please reload this application!", "cyan"))
-                exit()
+            session_key = input(colored("\nEnter your session-key (leave blank to keep existing) ", "yellow") + colored(">> ", "green"))
+            if session_key:
+                config['openai-key'] = {'session_key': session_key}
+                with open('config.ini', 'w') as configfile:
+                    config.write(configfile)
+                    clear()
+                    print(colored("Please reload this application!", "cyan"))
+                    exit()
         elif choice == "6":
             clear()
-            for i in range(len(bannercli)):
-                color = colors[i % len(colors)]
-                print(colored(bannercli[i], color), end="")
-            print(colored("\n\nAuthor: ", "red") + "@avencores")
-            print(colored("Telegram channel: ", "red") + "@hzfnews")
+            banner()
             try:
                 if 'openai-key' not in config:
                     clear()
-                    for i in range(len(bannercli)):
-                        color = colors[i % len(colors)]
-                        print(colored(bannercli[i], color), end="")
-                    print(colored("\n\nAuthor: ", "red") + "@avencores")
-                    print(colored("Telegram channel: ", "red") + "@hzfnews")
+                    banner()
                     print(colored("\nRead this article: ", "light_blue") + "https://lmmsoft.github.io/openai_update_check_balance_api/")
-                    session_key = input(colored("\nEnter your session-key: ", "yellow"))
+                    session_key = input(colored("\nEnter your session-key ", "yellow") + colored(">> ", "green"))
                     config['openai-key'] = {'session_key': session_key}
                     with open('config.ini', 'w') as configfile:
                         config.write(configfile)
@@ -277,22 +223,14 @@ def menu():
                 input()
             except Exception as e:
                 clear()
-                for i in range(len(bannercli)):
-                    color = colors[i % len(colors)]
-                    print(colored(bannercli[i], color), end="")
-                print(colored("\n\nAuthor: ", "red") + "@avencores")
-                print(colored("Telegram channel: ", "red") + "@hzfnews")
+                banner()
                 print(colored("\nERROR: ", "yellow") + str(e))
                 print(colored("\nPress ENTER to return to the main menu", "cyan"))
                 input()
                 menu()
         else:
             clear()
-            for i in range(len(bannercli)):
-                color = colors[i % len(colors)]
-                print(colored(bannercli[i], color), end="")
-            print(colored("\n\nAuthor: ", "red") + "@avencores")
-            print(colored("Telegram channel: ", "red") + "@hzfnews")
+            banner()
             print(colored("\nInvalid choice. Please try again.", "light_blue"))
             print(colored("\nPress ENTER to return to the main menu", "cyan"))
             input()
